8f109fdb89014e8718921820a8bc6b46c0515b07
hichamtalha6 <hichamtalha6@gmail.com>
2025-09-09 18:04:48 +0100

    Create joins_queries.sql

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,37 @@
+-- 1. INNER JOIN: Retrieve all bookings and the respective users who made those bookings
+SELECT 
+    b.booking_id,
+    b.property_id,
+    b.user_id,
+    b.start_date,
+    b.end_date,
+    u.name AS user_name,
+    u.email AS user_email
+FROM bookings b
+INNER JOIN users u ON b.user_id = u.user_id;
+
+
+-- 2. LEFT JOIN: Retrieve all properties and their reviews, including properties that have no reviews
+SELECT 
+    p.property_id,
+    p.title AS property_title,
+    p.location,
+    r.review_id,
+    r.user_id,
+    r.rating,
+    r.comment
+FROM properties p
+LEFT JOIN reviews r ON p.property_id = r.property_id;
+
+
+-- 3. FULL OUTER JOIN: Retrieve all users and all bookings,
+-- even if the user has no booking or a booking is not linked to a user
+SELECT 
+    u.user_id,
+    u.name AS user_name,
+    b.booking_id,
+    b.property_id,
+    b.start_date,
+    b.end_date
+FROM users u
+FULL OUTER JOIN bookings b ON u.user_id = b.user_id;
