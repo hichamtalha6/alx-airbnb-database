17a840f0c498ee64d88393eeabf402d9689a4cc6
hichamtalha6 <hichamtalha6@gmail.com>
2025-09-09 18:09:00 +0100

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -8,7 +8,8 @@ SELECT
     u.name AS user_name,
     u.email AS user_email
 FROM bookings b
-INNER JOIN users u ON b.user_id = u.user_id;
+INNER JOIN users u ON b.user_id = u.user_id
+ORDER BY b.booking_id;
 
 
 -- 2. LEFT JOIN: Retrieve all properties and their reviews, including properties that have no reviews
@@ -21,7 +22,8 @@ SELECT
     r.rating,
     r.comment
 FROM properties p
-LEFT JOIN reviews r ON p.property_id = r.property_id;
+LEFT JOIN reviews r ON p.property_id = r.property_id
+ORDER BY p.property_id, r.review_id;
 
 
 -- 3. FULL OUTER JOIN: Retrieve all users and all bookings,
@@ -34,4 +36,5 @@ SELECT
     b.start_date,
     b.end_date
 FROM users u
-FULL OUTER JOIN bookings b ON u.user_id = b.user_id;
+FULL OUTER JOIN bookings b ON u.user_id = b.user_id
+ORDER BY u.user_id, b.booking_id;
